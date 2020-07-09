3a2a51b52a4410d3ce49e2df0de34f115347774f
akhireddy9008 <54960581+akhireddy9008@users.noreply.github.com>
2020-07-08 17:52:49 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,23 +16,3 @@ module "rg_module" {
     environment = var.environment
 }
 
-module "VN_module"{
-    source = "./module/VNET"
-    ResourceName = module.rg_module.resource_name_output
-    location = module.rg_module.location_output
-    vn_address_space = var.vn_address_space
-    subnet_1 = var.subnet_1
-    subnet_2 = var.subnet_2
-    subnet_3 = var.subnet_3
-}
-
-module "VM_module" {
- source = "./module/VM"
- location = module.rg_module.location_output
- ResourceName = module.rg_module.resource_name_output
- #address_space = module.VN_module.vnet_cidr
- subnet_1 = module.VN_module.sub1_output
- public_ip_name = var.public_ip_name
- nic_name = var.nic_name
- vm_name = var.vm_name
-}
