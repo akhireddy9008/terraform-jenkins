68f1fc4630b1fd5f115a23b1665dd3dede95b35c
akhireddy9008 <54960581+akhireddy9008@users.noreply.github.com>
2020-07-08 17:50:45 -0700

    Add files via upload

---
+++ module/RG/output.tf
@@ -0,0 +1,9 @@
+output "resource_name_output" {
+    value = azurerm_resource_group.Rg.name
+}
+output "resource_id_output" {
+    value = azurerm_resource_group.Rg.id
+}
+output "location_output" {
+    value = azurerm_resource_group.Rg.location
+}
---
+++ module/RG/variable.tf
@@ -0,0 +1,8 @@
+variable "ResourceName" {
+    description = "Name of the Resource"
+}
+variable "location" {
+    description = "Name of the location"
+}
+variable "environment" {}
+
